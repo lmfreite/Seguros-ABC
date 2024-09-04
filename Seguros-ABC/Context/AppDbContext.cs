5f1b54655c034c0eb138067d0177af137c545037
Leonardo Freite <77871584+lmfreite@users.noreply.github.com>
2024-09-04 02:43:14 -0500

    creacion del contexto de la BD

--- Seguros-ABC/Context/AppDbContext.cs
+++ Seguros-ABC/Context/AppDbContext.cs
@@ -2,7 +2,7 @@
 using Seguros_ABC.Models;
 
 namespace Seguros_ABC.Context
-{
+{//inicializa la conexion a la BD
     public class AppDbContext : DbContext
     {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
